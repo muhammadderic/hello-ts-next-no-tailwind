f5930715bb0c7b1ec78a3ca084eb14b62c2fb31e
muhammadderic <muhammad.donny.e@gmail.com>
2024-09-24 22:41:34 +0700

    Setup authkit and add initial getUser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
-export default function Home() {
+import { getUser } from "@workos-inc/authkit-nextjs";
+
+export default async function Home() {
+  /*
+  await getUser():
+
+  { user: null }
+  */
+  const user = await getUser();
+  console.log(user);
+
   return (
     <h1>Hello Deric</h1>
   );
